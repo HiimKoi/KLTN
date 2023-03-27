7afad200e58a448ac32d486211228c2fe43d3e08
unknown <quochait1610@gmail.com>
2023-03-27 22:31:40 +0700

    Prevent brute login in function login user

--- classes/Login.php
+++ classes/Login.php
@@ -113,22 +113,66 @@ class Login extends DBConnection
 		}
 
 		$hash_password = md5($password);
-		// "SELECT * from clients where email = '$email' and password = md5('$password') ";
-		$qry = $this->conn->query("SELECT * from clients where email = '$email' and password = '$hash_password' ");
 
-		if ($qry->num_rows > 0) {
-			foreach ($qry->fetch_array() as $k => $v) {
-				$this->settings->set_userdata($k, $v);
-			}
-			$this->settings->set_userdata('login_type', 1);
-			$resp['status'] = 'success';
-		} else {
+		$qry = $this->conn->query("SELECT * from clients where email = '$email'");
+
+		if ($qry->num_rows <= 0) {
 			$resp['status'] = 'incorrect';
+			return json_encode($resp);
 		}
 
+		foreach ($qry->fetch_assoc() as $k => $v) {
+			$$k = $v;
+		}
+
+		// check failed >= 10 and compare last login time with current time (>= 5 minitue)
+		// define current time
+		$current_time = new DateTime('now');
+		$last_login_time = new DateTime($last_login);
+
+		$interval = $current_time->diff($last_login_time);
+		$minutes = $interval->format('%i');
+
+		// check time
+		if ($count_failed >= 5 && $minutes < 10) {
+			$resp['status'] = 'error';
+			$after_time = 10 - $minutes;
+			$resp['message'] = "Login sai nhiều lần, vui lòng thử lại sau $after_time phút.";
+			return json_encode($resp);
+		}
+
+		// continue check password correct => incorrect update last_login and count_failed to database
+		$last_login = $current_time->format('Y-m-d H:i:s');
+
+		if ($hash_password != $password) {
+			$count_failed += 1;
+			$sql_update = "UPDATE `clients` SET last_login = '$last_login', count_failed=$count_failed where email='$email'";
+			$this->conn->query($sql_update);
+
+			$resp['status'] = 'incorrect';
+			return json_encode($resp);
+		} else {
+			$count_failed = 0;
+			$sql_update = "UPDATE `clients` SET last_login = '$last_login', count_failed=$count_failed where email='$email'";
+			$this->conn->query($sql_update);
+		}
+
+		$this->settings->set_userdata("id", $id);
+		$this->settings->set_userdata("firstname", $firstname);
+		$this->settings->set_userdata("lastname", $lastname);
+		$this->settings->set_userdata("gender", $gender);
+		$this->settings->set_userdata("contact", $contact);
+		$this->settings->set_userdata("email", $email);
+		$this->settings->set_userdata("password", $password);
+		$this->settings->set_userdata("default_delivery_address", $default_delivery_address);
+		$this->settings->set_userdata("date_created", $date_created);
+		$this->settings->set_userdata("last_login", $last_login);
+
+		$resp['status'] = 'success';
 		return json_encode($resp);
 	}
 }
+
 $action = !isset($_GET['f']) ? 'none' : strtolower($_GET['f']);
 $auth = new Login();
 switch ($action) {
